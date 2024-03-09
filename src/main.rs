730ee2d3a2321e540edc7706fbc8faab390c9b97
Austin Gill <Notgnoshi@gmail.com>
2024-03-09 10:32:56 -0600

    Add CLI argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(about, verbatim_doc_comment, version)]
+struct CliArgs {
+    /// A path to a work tree or bare repository, or a clone URL
+    repository: String,
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn placeholder() {}
+fn main() {
+    let args = CliArgs::parse();
 }
