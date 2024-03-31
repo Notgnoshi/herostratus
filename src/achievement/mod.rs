f2fc8ef0e62ea0e54638bd474861f707d59c8494
Austin Gill <Notgnoshi@gmail.com>
2024-03-31 12:38:26 -0500

    Pull Rule out of rule processing iterator
    The process_rules() implementation already has enough going on. No need
    to make it even harder to understand.

--- src/achievement/mod.rs
+++ src/achievement/mod.rs
@@ -1,9 +1,9 @@
 //! The API that defines an achievement, and its parsing rules
 #[allow(clippy::module_inception)]
 mod achievement;
-mod rule;
+mod process_rules;
 #[cfg(test)]
-mod test_rule;
+mod test_process_rules;
 
-pub use achievement::Achievement;
-pub use rule::{process_rules, Rule};
+pub use achievement::{Achievement, Rule};
+pub use process_rules::process_rules;
--- src/achievement/process_rules.rs
+++ src/achievement/process_rules.rs
@@ -1,28 +1,4 @@
-use crate::achievement::Achievement;
-
-// TODO: How could user-contrib rule _scripts_ work? Consume commits via stdin, emit achievement
-// JSON on stdout?
-pub trait Rule {
-    /// Return the name of the [Achievement] that this rule generates
-    ///
-    /// There is expected to be a 1-1 correspondence between [Achievement]s and [Rule]s.
-    fn name(&self) -> &'static str;
-
-    /// Process the given [git2::Commit] to generate an [Achievement]
-    ///
-    /// Notice that this method takes `&mut self`. This is to allow the `Rule` to accumulate state
-    /// during commit processing. At the end of processing, [finalize](Self::finalize) will be
-    /// called, to generate any achievements from the accumulated state.
-    fn process(&mut self, commit: &git2::Commit, repo: &git2::Repository) -> Option<Achievement>;
-
-    /// Called when finished processing all commits
-    // TODO: There may need to be some lifetimes added / specified, so that implementors of the
-    // Rule can store the commit references? But I actually think storing the commits themselves
-    // isn't quite so useful?
-    fn finalize(&mut self, _repo: &git2::Repository) -> Vec<Achievement> {
-        Vec::new()
-    }
-}
+use crate::achievement::{Achievement, Rule};
 
 trait LoggedRule: Rule {
     /// Wrap Rule::process in a logging helper
