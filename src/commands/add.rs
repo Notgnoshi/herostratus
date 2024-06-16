6d54259f33f9e3c0c2818eb0fed3268062eeb778
Austin Gill <Notgnoshi@gmail.com>
2024-06-16 13:44:02 -0500

    Clone just the single branch, if given

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -4,14 +4,12 @@ use crate::cli::AddArgs;
 use crate::git::clone::{clone_repository, get_clone_path};
 
 pub fn add(args: &AddArgs, data_dir: &Path) -> eyre::Result<()> {
-    // TODO: Resolve the right clone path from the arguments (including config file?, CLI argument
-    // overrides)
-    //
-    // TODO: Resolve the right reference to process. If possible, only fetch the configured
-    // reference(s).
-
+    // TODO: Resolve the right clone path in the following priority order
+    // 1. args.path
+    // 2. configuration file
+    // 3. get_clone_path
     let clone_path = get_clone_path(data_dir, &args.url)?;
-    let _repo = clone_repository(&clone_path, &args.url, None, args.force)?;
+    let _repo = clone_repository(&clone_path, &args.url, args.branch.as_deref(), args.force)?;
 
     // TODO: Save the configuration parameters to a config file.
 
