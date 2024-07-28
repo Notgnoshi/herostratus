a225acbe6a3ac3f53497a73cc83552fd0e01955c
Austin Gill <Notgnoshi@gmail.com>
2024-07-28 14:49:17 -0500

    Pass 'add' subcommand arguments to the config file

--- src/commands/add.rs
+++ src/commands/add.rs
@@ -4,28 +4,47 @@ use crate::cli::AddArgs;
 use crate::config::{Config, RepositoryConfig};
 use crate::git::clone::{clone_repository, get_clone_path};
 
-pub fn add(args: &AddArgs, config: &mut Config, data_dir: &Path) -> eyre::Result<()> {
-    // TODO: What *should* the name be? The whole URL? Just the path part of the URL?
-    // TODO: What to do if the repository is already added?
-    let Some((_, name)) = args.url.rsplit_once('/') else {
-        eyre::bail!("Failed to parse URL '{}'", args.url)
-    };
-    let name = name.to_string();
-
-    let clone_path = if let Some(cli_path) = &args.path {
-        cli_path.to_path_buf()
-    } else {
-        get_clone_path(data_dir, &args.url)?
+fn args_to_config(args: &AddArgs, data_dir: &Path) -> eyre::Result<RepositoryConfig> {
+    // This default path is why we can't just 'impl From<AddArgs> for RepositoryConfig'
+    let path = match &args.path {
+        Some(path) => path.clone(),
+        None => get_clone_path(data_dir, &args.url)?,
     };
 
-    let repo_config = RepositoryConfig {
-        path: clone_path,
+    Ok(RepositoryConfig {
+        path,
         url: args.url.clone(),
         branch: args.branch.clone(),
-        ..Default::default()
+        remote_username: args.remote_username.clone(),
+        ssh_private_key: args.ssh_private_key.clone(),
+        ssh_public_key: args.ssh_public_key.clone(),
+        ssh_passphrase: args.ssh_passphrase.clone(),
+        https_password: args.https_password.clone(),
+    })
+}
+
+pub fn add(args: &AddArgs, config: &mut Config, data_dir: &Path) -> eyre::Result<()> {
+    // The name exists purely to make the TOML look pretty, with the added benefit of providing a
+    // unique handle for different (URL, Branch) pairs.
+    let name = match &args.name {
+        Some(name) => name.clone(),
+        None => {
+            let Some((_, name)) = args.url.rsplit_once('/') else {
+                eyre::bail!(
+                    "Failed to parse name from clone URL '{}'; no '/'s?",
+                    args.url
+                )
+            };
+            name.to_string()
+        }
     };
 
-    let _repo = clone_repository(&repo_config, args.force)?;
+    let repo_config = args_to_config(args, data_dir)?;
+
+    if !args.skip_clone {
+        let _repo = clone_repository(&repo_config, args.force)?;
+    }
+
     config.repositories.insert(name, repo_config);
     Ok(())
 }
--- tests/add.rs
+++ tests/add.rs
@@ -4,7 +4,7 @@ use common::CommandExt;
 use herostratus::config::{config_path, read_config, Config, RepositoryConfig};
 
 #[test]
-#[ignore = "Slow; performs git clone"]
+#[ignore = "Slow; Performs clone"]
 fn clone_herostratus() {
     let (mut cmd, temp) = common::herostratus(None);
     let data_dir = temp.as_ref().unwrap().path();
@@ -56,7 +56,7 @@ fn clone_herostratus() {
 }
 
 #[test]
-#[ignore = "Slow; performs git clone"]
+#[ignore = "Slow; Performs clone"]
 fn clone_herostratus_branch() {
     let (mut cmd, temp) = common::herostratus(None);
     let clone_dir = temp
@@ -97,4 +97,141 @@ fn clone_herostratus_branch() {
     let head = repo.head().unwrap();
     let head = head.name().unwrap();
     assert_eq!(head, "refs/heads/test/fixup");
+}
+
+#[test]
+#[ignore = "Slow; Performs clone; Requires SSH"]
+fn clone_herostratus_ssh() {
+    let (mut cmd, temp) = common::herostratus(None);
+    let clone_dir = temp
+        .as_ref()
+        .unwrap()
+        .path()
+        .join("git")
+        .join("Notgnoshi")
+        .join("herostratus.git");
+
+    let url = "git@github.com:Notgnoshi/herostratus.git";
+    cmd.arg("add").arg(url);
+
+    assert!(!clone_dir.exists());
+    assert!(!config_path(temp.as_ref().unwrap().path()).exists());
+
+    let output = cmd.captured_output().unwrap();
+    assert!(output.status.success());
+    assert!(clone_dir.exists());
+
+    let contents = std::fs::read_to_string(config_path(temp.as_ref().unwrap().path())).unwrap();
+    let expected = format!(
+        "[repositories.\"herostratus.git\"]\n\
+         path = \"{}\"\n\
+         url = \"git@github.com:Notgnoshi/herostratus.git\"\n\
+        ",
+        clone_dir.display()
+    );
+    assert_eq!(contents, expected);
+}
+
+#[test]
+fn add_the_same_repo_twice() {
+    let (mut cmd1, temp) = common::herostratus(None);
+    let (mut cmd2, _) = common::herostratus(Some(temp.as_ref().unwrap().path()));
+    let clone_dir = temp
+        .as_ref()
+        .unwrap()
+        .path()
+        .join("git")
+        .join("Notgnoshi")
+        .join("herostratus.git");
+
+    let url1 = "git@github.com:Notgnoshi/herostratus.git";
+    cmd1.arg("add").arg(url1).arg("--skip-clone");
+
+    let output1 = cmd1.captured_output().unwrap();
+    assert!(output1.status.success());
+
+    let contents = std::fs::read_to_string(config_path(temp.as_ref().unwrap().path())).unwrap();
+    let expected = format!(
+        "[repositories.\"herostratus.git\"]\n\
+         path = \"{}\"\n\
+         url = \"git@github.com:Notgnoshi/herostratus.git\"\n\
+        ",
+        clone_dir.display()
+    );
+    assert_eq!(contents, expected);
+
+    let url2 = "https://github.com/Notgnoshi/herostratus.git";
+    cmd2.arg("add").arg(url2).arg("--skip-clone");
+    let output2 = cmd2.captured_output().unwrap();
+    assert!(output2.status.success());
+
+    // The URL gets replaced
+    let contents = std::fs::read_to_string(config_path(temp.as_ref().unwrap().path())).unwrap();
+    let expected = format!(
+        "[repositories.\"herostratus.git\"]\n\
+         path = \"{}\"\n\
+         url = \"https://github.com/Notgnoshi/herostratus.git\"\n\
+        ",
+        clone_dir.display()
+    );
+    assert_eq!(contents, expected);
+}
+
+#[test]
+#[ignore = "Slow; Performs clone;"]
+fn two_branches_share_one_bare_repo() {
+    let (mut cmd1, temp) = common::herostratus(None);
+    let (mut cmd2, _) = common::herostratus(Some(temp.as_ref().unwrap().path()));
+
+    let clone_dir = temp
+        .as_ref()
+        .unwrap()
+        .path()
+        .join("git")
+        .join("Notgnoshi")
+        .join("herostratus.git");
+
+    let url = "https://github.com/Notgnoshi/herostratus.git";
+    cmd1.arg("add")
+        .arg(url)
+        .arg("--name")
+        .arg("herostratus-1")
+        .arg("test/simple");
+
+    let output1 = cmd1.captured_output().unwrap();
+    assert!(output1.status.success());
+
+    let contents = std::fs::read_to_string(config_path(temp.as_ref().unwrap().path())).unwrap();
+    let expected = format!(
+        "[repositories.herostratus-1]\n\
+         path = \"{}\"\n\
+         branch = \"test/simple\"\n\
+         url = \"https://github.com/Notgnoshi/herostratus.git\"\n\
+        ",
+        clone_dir.display()
+    );
+    assert_eq!(contents, expected);
+
+    cmd2.arg("add")
+        .arg(url)
+        .arg("test/fixup")
+        .arg("--name")
+        .arg("herostratus-2")
+        .arg("--skip-clone");
+
+    let output2 = cmd2.captured_output().unwrap();
+    assert!(output2.status.success());
+
+    // NOTE: The TOML file doesn't preserve order or comments, so parse the config file, and
+    // compare config values
+    let config = read_config(temp.as_ref().unwrap().path()).unwrap();
+    assert!(config.repositories.contains_key("herostratus-1"));
+    assert!(config.repositories.contains_key("herostratus-2"));
+
+    let config1 = &config.repositories["herostratus-1"];
+    let config2 = &config.repositories["herostratus-2"];
+    assert_eq!(config1.branch.as_deref(), Some("test/simple"));
+    assert_eq!(config2.branch.as_deref(), Some("test/fixup"));
+    assert_eq!(config1.path, config2.path);
+    assert_eq!(config1.url, config2.url);
 }
