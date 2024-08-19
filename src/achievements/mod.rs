86b870e372d2e5c2547a4809f7887d8a686663b4
Austin Gill <Notgnoshi@gmail.com>
2024-08-19 17:14:07 -0500

    Rename fixup.rs -> h001_fixup.rs for ease of future growth

--- src/achievements/mod.rs
+++ src/achievements/mod.rs
@@ -1,11 +1,11 @@
 //! The achievements builtin to Herostratus
 // TODO: Figure out an easier / better way to organize rules
-mod fixup;
+mod h001_fixup;
 
 use crate::achievement::Rule;
 
 pub fn builtin_rules() -> Vec<Box<dyn Rule>> {
     // TODO: Rule factory? How to get Rules to register themselves? Through a static singleton +
     // module ctor? Metaprogramming? Proc Macro?
-    vec![Box::new(fixup::IMeantToFixThatUpLaterISwear) as Box<dyn Rule>]
+    vec![Box::new(h001_fixup::Fixup) as Box<dyn Rule>]
 }
