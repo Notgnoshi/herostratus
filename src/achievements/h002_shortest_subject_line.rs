1e2c0b614da16407a21532d81ff6d98d979b4085
Austin Gill <Notgnoshi@gmail.com>
2024-08-19 19:18:52 -0500

    Add test for retaining state between repositories
    I think it's too error prone to require each Rule implementation to
    remember to reset its internal state on calls to `finalize()`. It would
    be better to re-create new impl Rule objects for each repository.
    
    This is what crate::achievement::process_rules::grant() does. But it's a
    bit inflexible (especially when I get to customizing the rules that are
    run, and injecting user-contrib rules, and opinionated ones).
    
    But then this gets into how crate::achievements::builtin_rules() works,
    which I want to change up to use something more akin to a plugin
    registry (no centralized list, rules can register themselves) using the
    inventory crate.
    
    But I ran into lots of problems with that, because Rule implementations
    need to be mutable so they can store state, but also they need to be
    Sized trait objects (so Box<dyn Rule>) except that inventory requires
    const fns, which Box::new() isn't.
    
    So I think the plan is to use inventory to register and collect Rule
    factories, rather than the Rules themselves. But that's pretty tricky,
    and needs to change up how the main application iteration works.
    
    This will also impact how parallelism works, when I get to it.

--- src/achievements/h002_shortest_subject_line.rs
+++ src/achievements/h002_shortest_subject_line.rs
@@ -88,4 +88,32 @@ mod tests {
         let commit = repo.repo.find_commit(oid).unwrap();
         assert_eq!(commit.summary(), Some("1234"));
     }
+
+    #[test]
+    fn test_resets_state_between_repositories() {
+        let repo1 =
+            fixtures::repository::with_empty_commits(&["0123456789", "1234567", "234"]).unwrap();
+        let repo2 =
+            fixtures::repository::with_empty_commits(&["1234567890", "2345671", "1234"]).unwrap();
+
+        let rules1 = vec![Box::new(ShortestSubjectLine::default()) as Box<dyn Rule>];
+        // grant_with_rules() consumes the rules Vec, so there _can't_ be any state held between
+        // processing any two repositories
+        let rules2 = vec![Box::new(ShortestSubjectLine::default()) as Box<dyn Rule>];
+
+        let achievements1 = grant_with_rules("HEAD", &repo1.repo, rules1).unwrap();
+        let achievements2 = grant_with_rules("HEAD", &repo2.repo, rules2).unwrap();
+        let achievements1: Vec<_> = achievements1.collect();
+        assert_eq!(achievements1.len(), 1);
+        let achievements2: Vec<_> = achievements2.collect();
+        assert_eq!(achievements2.len(), 1);
+
+        let oid = achievements1[0].commit;
+        let commit = repo1.repo.find_commit(oid).unwrap();
+        assert_eq!(commit.summary(), Some("234"));
+
+        let oid = achievements2[0].commit;
+        let commit = repo2.repo.find_commit(oid).unwrap();
+        assert_eq!(commit.summary(), Some("1234"));
+    }
 }
