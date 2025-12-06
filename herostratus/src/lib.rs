534104843bf0ffd28cfb4f701a9ef5f3bb1d8382
Austin Gill <Notgnoshi@gmail.com>
2025-12-06 11:35:58 -0600

    Hoist is_equal_ignoring_whitespace into a utility module

--- herostratus/src/lib.rs
+++ herostratus/src/lib.rs
@@ -4,5 +4,6 @@ pub mod commands;
 pub mod config;
 pub mod git;
 pub mod rules;
+pub mod utils;
 
 pub use gix::bstr;
--- herostratus/src/rules/h006_whitespace_only.rs
+++ herostratus/src/rules/h006_whitespace_only.rs
@@ -1,5 +1,6 @@
 use crate::achievement::{Achievement, Rule, RuleFactory};
 use crate::bstr::BStr;
+use crate::utils::utf8_whitespace::is_equal_ignoring_whitespace;
 
 #[derive(Default)]
 pub struct WhitespaceOnly;
@@ -158,158 +159,10 @@ fn on_modification(
     let before_s = BStr::new(&before.data);
     let after_s = BStr::new(&after.data);
 
-    // tracing::debug!("Diffing {before:?} and {after:?}");
-    // tracing::debug!("Before: {before_s:?}");
-    // tracing::debug!("After: {after_s:?}");
-
     if !is_equal_ignoring_whitespace(before_s, after_s) {
         *found_non_whitespace = true;
         Ok(gix::object::tree::diff::Action::Cancel)
     } else {
         Ok(gix::object::tree::diff::Action::Continue)
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum CharOrByte {
-    Char(char),
-    Byte(u8),
-}
-
-struct WhiteSpaceSkipper<'a, I> {
-    chunks: &'a mut I,
-    current_valid: std::str::Chars<'a>,
-    current_invalid: std::slice::Iter<'a, u8>,
-}
-
-// This might not be very cache or branch predictor friendlt, but it's simple enough until
-// performance becomes a concern.
-impl<'a, I> WhiteSpaceSkipper<'a, I>
-where
-    I: Iterator<Item = std::str::Utf8Chunk<'a>>,
-{
-    pub fn new(chunks: &'a mut I) -> Self {
-        let mut this = Self {
-            chunks,
-            current_valid: "".chars(),
-            current_invalid: [].iter(),
-        };
-        this.advance_chunk();
-        this
-    }
-
-    fn advance_chunk(&mut self) {
-        if let Some(chunk) = self.chunks.next() {
-            self.current_valid = chunk.valid().chars();
-            self.current_invalid = chunk.invalid().iter();
-        }
-    }
-
-    fn next_any_char(&mut self) -> Option<CharOrByte> {
-        if let Some(c) = self.current_valid.next() {
-            return Some(CharOrByte::Char(c));
-        }
-
-        if let Some(b) = self.current_invalid.next() {
-            return Some(CharOrByte::Byte(*b));
-        }
-
-        self.advance_chunk();
-
-        if let Some(c) = self.current_valid.next() {
-            return Some(CharOrByte::Char(c));
-        }
-
-        if let Some(b) = self.current_invalid.next() {
-            return Some(CharOrByte::Byte(*b));
-        }
-
-        None
-    }
-
-    fn next_non_whitespace(&mut self) -> Option<CharOrByte> {
-        while let Some(c) = self.next_any_char() {
-            match c {
-                CharOrByte::Char(c) if c.is_whitespace() => continue,
-                CharOrByte::Byte(b) if b.is_ascii_whitespace() => continue,
-                _ => return Some(c),
-            }
-        }
-        None
-    }
-}
-
-impl<'a, I> Iterator for WhiteSpaceSkipper<'a, I>
-where
-    I: Iterator<Item = std::str::Utf8Chunk<'a>>,
-{
-    type Item = CharOrByte;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.next_non_whitespace()
-    }
-}
-
-/// Do a two-finger comparison of `a` and `b` skipping over all whitespace
-fn is_equal_ignoring_whitespace<A: AsRef<gix::bstr::BStr>, B: AsRef<gix::bstr::BStr>>(
-    a: A,
-    b: B,
-) -> bool {
-    let mut a_chunks = a.as_ref().utf8_chunks();
-    let mut a_chars = WhiteSpaceSkipper::new(&mut a_chunks);
-    let mut b_chunks = b.as_ref().utf8_chunks();
-    let mut b_chars = WhiteSpaceSkipper::new(&mut b_chunks);
-    loop {
-        let a = a_chars.next();
-        let b = b_chars.next();
-
-        match (a, b) {
-            // Both iterators exhausted
-            (None, None) => return true,
-            // Both have a non-whitespace character and they are equal
-            (Some(ac), Some(bc)) if ac == bc => continue,
-            // Otherwise they're not equal, or one iterator is exhausted before the other
-            _ => return false,
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn bstr_equal_ignoring_whitespace() {
-        let a = b"";
-        let b = b"";
-        assert!(is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let a = b"a";
-        let b = b"a";
-        assert!(is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let a = b"a";
-        let b = b"b";
-        assert!(!is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let s = b"\xC2\xA0\t \r\n ";
-        let s = str::from_utf8(s).unwrap();
-        assert!(s.chars().all(|c| c.is_whitespace()));
-
-        let a = b"   a\t";
-        let b = b"\xC2\xA0a\n\t \r\n "; // \u{A0} is non-breaking space
-        assert!(is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let a = b"a\xFF b";
-        let b = b"ab";
-        assert!(!is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let a = b"a\xFF b";
-        let b = b"a\xFFb";
-        assert!(is_equal_ignoring_whitespace(&a[..], &b[..]));
-
-        let a = b"a\xFF b";
-        let b = b"ac\xFF b";
-        assert!(!is_equal_ignoring_whitespace(&a[..], &b[..]));
     }
 }
