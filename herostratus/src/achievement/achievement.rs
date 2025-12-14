ca91f34923df1a754dcf8d5ea2a62879f22327ca
Austin Gill <Notgnoshi@gmail.com>
2025-12-13 20:24:57 -0600

    Add diff handlers to Rule

--- herostratus/src/achievement/achievement.rs
+++ herostratus/src/achievement/achievement.rs
@@ -149,6 +149,44 @@ pub trait Rule {
     fn finalize(&mut self, _repo: &gix::Repository) -> Vec<Achievement> {
         Vec::new()
     }
+
+    /// Indicates whether this [Rule] would like to receive commit diffs
+    ///
+    /// If a rule is interested in diffs, then for each commit processed, the following methods
+    /// will be called in order:
+    /// 1. [process](Self::process)
+    /// 2. [on_diff_start](Self::on_diff_start)
+    /// 3. [on_diff_change](Self::on_diff_change) for each change
+    /// 4. [on_diff_end](Self::on_diff_end)
+    ///
+    /// If `on_diff_change` returns `Action::Cancel`, or an `Err`, no further changes will be
+    /// passed to the rule for that commit. This acts as an early-out mechanism to save on
+    /// computation.
+    fn is_interested_in_diffs(&self) -> bool {
+        false
+    }
+
+    /// Start the diff for the given commit
+    fn on_diff_start(&mut self, _commit: &gix::Commit, _repo: &gix::Repository) {}
+
+    /// Process a single change from the diff
+    ///
+    /// If this method returns `Action::Cancel`, no further changes will be passed to the rule
+    fn on_diff_change(
+        &mut self,
+        _commit: &gix::Commit,
+        _repo: &gix::Repository,
+        _change: gix::object::tree::diff::Change,
+    ) -> eyre::Result<gix::object::tree::diff::Action> {
+        Ok(gix::object::tree::diff::Action::Cancel)
+    }
+
+    /// Handle the end of the diff for the given commit
+    ///
+    /// Will be called regardless of the return value for `on_diff_change`
+    fn on_diff_end(&mut self, _commit: &gix::Commit, _repo: &gix::Repository) -> Vec<Achievement> {
+        Vec::new()
+    }
 }
 
 /// Wrap achievement granting in logging
